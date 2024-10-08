273297fcdc37e9ab1512bab6fcb0c9df92c784ce
Gavin Purdie <Purdie-G@ulster.ac.uk>
2024-10-08 10:09:54 +0100

    changes

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -5,14 +5,19 @@ class Book
     string Title;
     string Author;
     string ISBN;
+    int NoOfPages;
 
     void DisplayInfo()
     {
         //Output the book information to the console
 
+        Console.WriteLine($"Book Information");
+        Console.WriteLine($"----------------");
         Console.WriteLine($"Book title: {Title}");
         Console.WriteLine($"Book Author: {Author}");
         Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"No of pages: {NoOfPages}");
+        Console.WriteLine();
     }
 
 
@@ -24,11 +29,13 @@ class Book
         book.Title = "C# for begginers";
         book.Author = "Bill Gates";
         book.ISBN = "123456677";
+        book.NoOfPages = 200;
 
         Book book2 = new Book();
         book2.Title = "C# methods and classes";
         book2.Author = "Microsoft";
         book2.ISBN = "1234566";
+        book2.NoOfPages = 50;
 
         //Output Book information to the console
         book.DisplayInfo();
