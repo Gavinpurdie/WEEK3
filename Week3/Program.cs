6b9d646279ce9354da1649723b8503c4efdc9c7c
Gavin Purdie <Purdie-G@ulster.ac.uk>
2024-10-08 10:00:58 +0100

    Created new classes

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+        //Output the book information to the console
+
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+    }
+
+
+    static void Main(string[] args)
+    {
+        //Create a new instance of the book class
+        
+        Book book = new Book();
+        book.Title = "C# for begginers";
+        book.Author = "Bill Gates";
+        book.ISBN = "123456677";
+
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "1234566";
+
+        //Output Book information to the console
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+
+
+
+
+
+    }
+}
